23ac6b51dcf89cd220864da64aa653be86c47049
Jacob577 <deltora.ericsson@gmail.com>
2021-01-19 20:37:22 +0100

    update

--- Second_project/src/BytesShortintLong.java
+++ Second_project/src/BytesShortintLong.java
@@ -8,10 +8,49 @@ public class BytesShortintLong {
 //        System.out.println("Minimum " + myMin);
 //        System.out.println("myMax " + myMax);
 
-        byte myMinByteValue = Byte.MIN_VALUE;
-        byte myMaxByteValue = Byte.MAX_VALUE;
-        System.out.println("Byte Minimyum value = " + myMinByteValue);
-        System.out.println("Byte Maximum value = " + myMaxByteValue);
+//        byte myMinByteValue = Byte.MIN_VALUE;
+//        byte myMaxByteValue = Byte.MAX_VALUE;
+//        System.out.println("Byte Minimyum value = " + myMinByteValue);
+//        System.out.println("Byte Maximum value = " + myMaxByteValue);
+
+        //short can hold 16 bit
+        //int can hold 32 bit
+        //byte can hold 8 bit
+        //Long has a size of 64 bits
+//        long myLongValue = 100L;
+//
+//        long myMinByteValue = Long.MIN_VALUE;
+//        long myMaxByteValue = Long.MAX_VALUE;
+//        System.out.println("Byte Minimyum value = " + myMinByteValue);
+//        System.out.println("Byte Maximum value = " + myMaxByteValue);
+
+//        int myIntValue = 5;
+//        int myTotal = (myIntValue/2);
+//
+//        byte myNewByteValue = (byte)(myTotal/2);
+
+        //Casting is when I can put datatype in front of the value to use it as another
+
+//        byte myByte = 100;
+//        short myShort = 11111;
+//        int myint = 10000;
+//        long myLong = (50000L + 10L*(long)(myint + myByte + myShort));
+//
+//        System.out.println(myLong);
+
+        //float can hold 32 bit
+        //double can hold 64 bit
+
+//        int myInt = 5/2;
+//        float myFloat = 5f/2f;
+//        double myDouble = 5d/2d;
+//
+//        System.out.println(myInt);
+//        System.out.println(myFloat);
+//        System.out.println(myDouble);
+
+        double amountOfPounds = 10d;
+        System.out.println(amountOfPounds/0.45359237d);
 
     }
 }
