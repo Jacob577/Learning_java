eee854da8597b4cd900b244091f6c2a0f12a8115
Jacob577 <deltora.ericsson@gmail.com>
2021-01-19 21:41:11 +0100

    end of workout

--- Second_project/src/BytesShortintLong.java
+++ Second_project/src/BytesShortintLong.java
@@ -49,8 +49,76 @@ public class BytesShortintLong {
 //        System.out.println(myFloat);
 //        System.out.println(myDouble);
 
-        double amountOfPounds = 10d;
-        System.out.println(amountOfPounds/0.45359237d);
+//        double amountOfPounds = 10d;
+//        System.out.println(amountOfPounds/0.45359237d);
+
+        //Can only store one letter per char
+//        char myChar = 'D';
+//        char myUnicode = '\u0044';
+//
+//        System.out.println(myUnicode);
+
+//        String myString = "This is a string";
+//
+//        System.out.println(myString);
+
+        //********************************************
+        //Operators
+//        ***********
+//        int result = 1+2; //=3
+
+//        int result = 1;
+//        result++;
+//        System.out.println(result);
+//
+//        result *= 10;
+//        System.out.println(result);
+
+//        boolean isAlien = false;
+////        if (isAlien == false){
+////            System.out.println("is ok");
+//        }
+
+//        int topScore = 80;
+//        if (topScore == 100){
+//            System.out.println(topScore==100);
+//        }
+//
+//        int secondTopScore = 60;
+//        if ((topScore > secondTopScore) && (topScore <100)){
+//            System.out.println("This worked");
+//        }
+//
+//        if ((topScore>90) || secondTopScore <= 90){
+//            System.out.println("Second stuff works");
+//        }
+
+//        int newValue = 50;
+//        if (newValue == 50){
+//            System.out.println("This is a test of equals operator");
+//        }
+
+        //If a boolean in if: if (var=true = true) works because if wants a book. == returns a bool
+        //Recommended to use !isCar in if statement
+
+        //? checks if isCar is true, otherwise it assigns wasCar with false,
+        //Shortening for if IsCar == true: was car = true, else wasCar = false
+//        boolean isCar = false;
+//        boolean wasCar = isCar ? true : false;
+//        if (wasCar){
+//            System.out.println("Was car is true");
+
+        double myFirstDouble = 20.00d;
+        double mySecondDouble = 80.00d;
+        double result = (myFirstDouble + mySecondDouble)*100d;
+
+        double remainder = result%40;
+
+        boolean isRemainder = (remainder == 0) ? true : false;
+
+        if (!isRemainder){
+            System.out.println("There was some remainder!");
+        }
 
     }
 }
