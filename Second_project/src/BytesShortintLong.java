ba9d1797a13e1e47712ab6226bd65bbe0f9c2d86
Jacob577 <deltora.ericsson@gmail.com>
2021-01-19 20:11:13 +0100

    A commit

---
+++ Second_project/src/BytesShortintLong.java
@@ -0,0 +1,17 @@
+public class BytesShortintLong {
+    public static void main(String[] args) {
+
+//        int myValue = 10000;
+//        int myMin = Integer.MIN_VALUE;
+//        int myMax = Integer.MAX_VALUE;
+
+//        System.out.println("Minimum " + myMin);
+//        System.out.println("myMax " + myMax);
+
+        byte myMinByteValue = Byte.MIN_VALUE;
+        byte myMaxByteValue = Byte.MAX_VALUE;
+        System.out.println("Byte Minimyum value = " + myMinByteValue);
+        System.out.println("Byte Maximum value = " + myMaxByteValue);
+
+    }
+}
---
+++ src/Hello.java
@@ -0,0 +1,14 @@
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 6;
+        int myThirdNumber = 12;
+
+
+        int myTotal = myThirdNumber + mySecondNumber + myFirstNumber;
+        int myLasOne = (myTotal - 1000);
+        System.out.println(myLasOne);
+    }
+}
