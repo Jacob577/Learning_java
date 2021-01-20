480665df4fe530dd28229dd958feadcdbf6c6b1f
Jacob577 <deltora.ericsson@gmail.com>
2021-01-20 21:09:45 +0100

    Coding for today, keywords with more

---
+++ Section4/Proj1/src/Proj1.java
@@ -0,0 +1,16 @@
+public class Proj1 {
+    public static void main(String[] args) {
+        // Keywords and expressions
+//        int myInt = 40;
+//
+//        if (myInt==40){
+//            System.out.println("This integer is ok!");
+//
+//        } else {
+//            System.out.println("This integer is not ok!");
+//        }
+
+        int myVariabele = 50;
+         //Java is not concerned with spacing or indentation compared to Python
+    }
+}
