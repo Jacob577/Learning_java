d10467b29bb755371d6853d63194c703131fc739
Jacob577 <deltora.ericsson@gmail.com>
2021-01-23 16:00:18 +0100

    Daily coding

---
+++ Section5/Excersices1/src/Main.java
@@ -0,0 +1,68 @@
+public class Main {
+    public static void main(String[] args) {
+
+//        int count = 0;
+//        int summarizing = 0;
+//        for (int i = 1; i<= 1000; i += 1){
+//            if (i%3 == 0 && i%5 == 0){
+//                count ++;
+//                summarizing += i;
+//            }
+//            if (count >= 5){
+//                System.out.println("The sum is: " + summarizing);
+//                break;
+//            }
+//        }
+//        boolean doneForToday = true;
+//        boolean isFriday = true;
+//        int bear = 5;
+//        int count = 0;
+//        while (count != 5){
+//            System.out.println("Count value is equal of: " + count);
+//            count ++;
+//        }
+
+//        if (doneForToday) {
+//            if (isFriday){
+//                System.out.println("Drink beer");
+//            } else if (!isFriday){
+//                System.out.println("Drink a little beer");
+//            }
+//        }else doneForToday = true;
+//        int count = 0;
+//        do {
+//            System.out.println("Count value was "+count);
+//            count ++;
+//        } while (count != 6);
+
+        
+    }
+
+
+
+
+
+
+//    public static boolean isOdd(int number){
+//        if (number <= 0){
+//            return false;
+//        } else if (number%2 != 0){
+//            return true;
+//        } return false;
+//    }
+//    public static int sumOdd(int first, int second){
+//        if (first <=0 || second <= 0){
+//            return -1;
+//        } else if (second < first){
+//            return -1;
+//        }
+//        int summarized = 0;
+//        for (int i = first; i<=second;i++){
+//            if (isOdd(i)){
+//                summarized += i;
+//            }
+//
+//        }
+//        return summarized;
+//    }
+}
---
+++ Section5/part1/src/Main.java
@@ -0,0 +1,147 @@
+import java.util.Locale;
+
+public class Main {
+    public static void main(String[] args) {
+//        for (int i = 0; i<=8; i++){
+//            System.out.println("The total earned amount with interest rate of " + i + "% is:");
+//            System.out.println(String.format("%.2f",calculateInterest(1000,i)));
+//            //String.format can be used to print out the number with just a certain amount of decimals.
+//        }
+//        for (int i = 8; i>=2; i--){
+//            System.out.println("The total earned amount with interest rate of " + i + "% is:");
+//            System.out.println(String.format("%.2f",calculateInterest(1000,i)));
+//            //String.format can be used to print out the number with just a certain amount of decimals.
+//        }
+//            int count = 0;
+//            for (int i = 10; i<1000; i++){
+//                System.out.println("Testing: " + i);
+//                if (isPrime(i)){
+//                    count ++;
+//                    System.out.println(i + " is a prime number!");
+//                }
+//                if (count >= 3){
+//                    break;
+//                }
+//            }
+//        int switchValue = 5;
+//        switch(switchValue) {
+//            case 1:
+//                System.out.println("Value was 1");
+//                break;
+//            case 2:
+//                System.out.println("Value was 2");
+//                break;
+//            case 3: case 4: case 5:
+//                System.out.println("Was a 3, 4 or 5");
+//            default:
+//                System.out.println("Was not 1 or 2");
+                //Remember to have breaks after break, use switch before case!
+//        char char1='d';
+//        switch (char1){
+//            case 'a':
+//                System.out.println("Variable was a");
+//                break;
+//            case 'b':
+//                System.out.println("cahr was b");
+//                break;
+//            case 'c':
+//                System.out.println("cahr was c");
+//                break;
+//            case 'd':
+//                System.out.println("cahr was d");
+//                break;
+//            case 'e':
+//                System.out.println("cahr was e");
+//                break;
+//            default:
+//                System.out.println("No match");
+//                break;
+//
+//        }
+        //There is a method to strings ex:
+//        String month = "January";
+//        System.out.println("The month is " + month.toUpperCase());
+//        printDayOfTheWeek(-1);
+
+    }
+//    public static void printDayOfTheWeek(int day){
+//        switch (day){
+//            case 0:
+//                System.out.println("Monday");
+//                break;
+//            case 1:
+//                System.out.println("Tuesday");
+//                break;
+//            case 2:
+//                System.out.println("Wednesday");
+//                break;
+//            case 3:
+//                System.out.println("Thursday");
+//                break;
+//            case 4:
+//                System.out.println("Friday");
+//                break;
+//            case 5:
+//                System.out.println("Saturday");
+//                break;
+//            case 6:
+//                System.out.println("Sunday");
+//                break;
+//            default:
+//                System.out.println("Invalid day");
+//        }
+//    }
+
+//    public static void printNumberInWord(int number){
+//        switch (number){
+//            case 0:
+//                System.out.println("ZERO");
+//                break;
+//            case 1:
+//                System.out.println("ONE");
+//                break;
+//            case 2:
+//                System.out.println("TWO");
+//                break;
+//            case 3:
+//                System.out.println("THREE");
+//                break;
+//            case 4:
+//                System.out.println("FOUR");
+//                break;
+//            case 5:
+//                System.out.println("FIVE");
+//                break;
+//            case 6:
+//                System.out.println("SIX");
+//                break;
+//            case 7:
+//                System.out.println("SEVEN");
+//                break;
+//            case 8:
+//                System.out.println("EIGHT");
+//                break;
+//            case 9:
+//                System.out.println("NINE");
+//                break;
+//            default:
+//                System.out.println("OTHER");
+//        }
+//    }
+//public static double calculateInterest(double amount, double interestRate){
+//        return (amount*interestRate/100);
+//}
+//
+//public static boolean isPrime(int n){
+//        if (n==1) {
+//            return false;
+//        }
+//        for (int i=2; i <= n/2; i++){
+//            if (n%i == 0) {
+//                return false;
+//            }
+//        }
+//    return true;
+//    }
+
+}
